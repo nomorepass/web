e92e61cc88a32c4f80e8e018a7cdc328a6a2416d
isayme <isaymeorg@gmail.com>
2017-01-20 07:11:38 +0800

    half & center ui

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,21 +1,23 @@
 <template>
-  <div>
-    <label class="label">Email</label>
-    <p class="control">
-      <input class="input is-medium" type="text" placeholder="email" v-model="email">
-    </p>
-    <label class="label">Password</label>
-    <p class="control">
-      <input class="input is-medium" type="password" placeholder="password" v-model="password">
-    </p>
+  <div class="columns">
+    <div class="column is-half is-offset-3">
+      <label class="label">Email</label>
+      <p class="control">
+        <input class="input is-medium" type="text" placeholder="email" v-model="email">
+      </p>
+      <label class="label">Password</label>
+      <p class="control">
+        <input class="input is-medium" type="password" placeholder="password" v-model="password">
+      </p>
 
-    <div class="control is-grouped">
-      <p class="control">
-        <button class="button is-primary" @click="login">LOGIN</button>
-      </p>
-      <p class="control">
-        <button class="button" @click="gotoSignup">SINGUP</button>
-      </p>
+      <div class="control is-grouped">
+        <p class="control">
+          <button class="button is-primary" @click="login">LOGIN</button>
+        </p>
+        <p class="control">
+          <button class="button" @click="gotoSignup">SINGUP</button>
+        </p>
+      </div>
     </div>
   </div>
 </template>
--- src/components/signup.vue
+++ src/components/signup.vue
@@ -1,27 +1,29 @@
 <template>
-  <div>
-    <label class="label">Username</label>
-    <p class="control">
-      <input class="input is-medium" type="text" placeholder="username" v-model="username">
-    </p>
-
-    <label class="label">Email</label>
-    <p class="control">
-      <input class="input is-medium" type="text" placeholder="email" v-model="email">
-    </p>
-
-    <label class="label">Password</label>
-    <p class="control">
-      <input class="input is-medium" type="password" placeholder="password" v-model="password">
-    </p>
-
-    <div class="control is-grouped">
+  <div class="columns">
+    <div class="column is-half is-offset-3">
+      <label class="label">Username</label>
       <p class="control">
-        <button class="button is-primary" @click="signup">SIGNUP</button>
+        <input class="input is-medium" type="text" placeholder="username" v-model="username">
       </p>
+
+      <label class="label">Email</label>
       <p class="control">
-        <button class="button" @click="gotoLogin">LOGIN</button>
+        <input class="input is-medium" type="text" placeholder="email" v-model="email">
       </p>
+
+      <label class="label">Password</label>
+      <p class="control">
+        <input class="input is-medium" type="password" placeholder="password" v-model="password">
+      </p>
+
+      <div class="control is-grouped">
+        <p class="control">
+          <button class="button is-primary" @click="signup">SIGNUP</button>
+        </p>
+        <p class="control">
+          <button class="button" @click="gotoLogin">LOGIN</button>
+        </p>
+      </div>
     </div>
   </div>
 </template>
